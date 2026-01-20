5512fd614d5dc92409e5b6eb2262eeaa1acdc3a5
mattb9087 <matbouqui@gmail.com>
2026-01-20 18:53:13 -0500

    Created environment

--- simulate.py
+++ simulate.py
@@ -1,4 +1,11 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for i in range(0, 1000):
+    p.stepSimulation()
+    time.sleep(0.01)
+    print(i)
+
 p.disconnect()
