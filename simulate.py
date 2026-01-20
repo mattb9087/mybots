93d51bca4eb697373c1488e848a6e28c164f1285
mattb9087 <matbouqui@gmail.com>
2026-01-20 18:57:57 -0500

    creating physics client

--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 for i in range(0, 1000):
     p.stepSimulation()
     time.sleep(0.01)
